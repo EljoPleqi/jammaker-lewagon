f670bca555f455fcdbd2197d15160b4465a3a732
eljopleqi <eljopleqi@gmail.com>
2022-05-31 16:08:28 +0200

    added the begining of the fetch functionality

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
 
   root to: 'pages#home'
   get "/scraper", to: "pages#scraper"
+  post "/query", to: 'pages#fetch'
+  get "/query", to: 'pages#query'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :dashboard, only: [:show] do
--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-
 ActiveRecord::Schema.define(version: 2022_05_31_085746) do
 
   # These are extensions that must be enabled in order to support this database
@@ -71,13 +69,10 @@ ActiveRecord::Schema.define(version: 2022_05_31_085746) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-
-    t.string "username"
-    t.string "image"
-
     t.string "provider"
     t.string "uid"
     t.string "username"
+    t.string "image"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
