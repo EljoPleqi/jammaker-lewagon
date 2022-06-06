c8a6b9656280366621461dca5f3245ddc301c226
eljopleqi <eljopleqi@gmail.com>
2022-06-06 14:46:45 +0200

    added the new recipe-controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,6 @@
 class RecipesController < ApplicationController
-  require "open-uri"
-  before_action :find_user, only: %i[create_playlist fetch]
+  require 'rest-client'
+  before_action :spotify_urls, only: [:fetch_songs]
 
   def index
     @recipes = Recipe.all
@@ -13,15 +13,16 @@ class RecipesController < ApplicationController
     @recipe.user = current_user
     @recipe.save
     @instructions = Instruction.parse(@recipe.steps)
-    @instructions.shift
     @instructions.each do |instruction|
       Instruction.create(content: instruction, recipe: @recipe)
     end
-    # create_playlist(@recipe.preptime.to_i, @recipe.title)
+    create_playlist(@recipe.preptime.to_i, @recipe.title)
     redirect_to recipe_path(@recipe)
   end
 
   def show
+    # find recipes to show
+    # show all of the instructions for that recipes
     @recipe = Recipe.find(params[:id])
     @instructions = @recipe.instructions
   end
@@ -36,52 +37,77 @@ class RecipesController < ApplicationController
   # * the #create_playlist takes two paraments the spotify user and the prep_time from the scrapper
   # * the #create_playlist generates and populates the user recipe
   def create_playlist(prep_time, playlist_name)
-    user_hash = JSON.parse(current_user.spotify_hash)
-    spotify_user = RSpotify::User.new(user_hash)
-    # * CREATE THE PLAYLIST
-    playlist = spotify_user.create_playlist!("Jammaker-#{playlist_name}")
+
     # * currate the songs array, it must hold either tracks or a collection of strings that is a valid spotify track uri
     songs = []
     # TODO: calculate the total duration of all the songs inside the songs array
     playlist_time = 0
     # * looping until the total playlist time reaches the total preptime
-    until playlist_time == prep_time
+    until playlist_time >= prep_time
       # TODO: loop logic
-      song = fetch("punk")
-      playlist_time += song[0][1] / 60_000 unless song.nil?
-      songs.push(song[0][0])
+      song = fetch_songs
+      playlist_time += song[1] / 60_000 unless song.nil?
+      songs.push(song[0])
     end
+    # * CREATE THE PLAYLIST
 
+    spotify_user = RSpotify::User.new(JSON.parse(current_user.spotify_hash))
+    playlist = spotify_user.create_playlist!("Jammaker-#{playlist_name}")
     playlist.add_tracks!(songs)
-
-    @message = ["Playlist all done 🎉 total playlist time => #{playlist_time}"] # * <--- RETURN
   end
 
-  def fetch(cat)
+  def fetch_songs
+    # * get the categories
     user_hash = JSON.parse(current_user.spotify_hash)
-    spotify_user = RSpotify::User.new(user_hash)
+    enc_credentials = "Bearer #{user_hash['credentials']['token']}"
+  #   categories = RestClient::Request.new(
+  #   {
+  #     url: spotify_urls[:categories],
+  #     method: "GET",
+  #     headers: {"Accept" => "application/json", "Content-Type" => "application/json", "Authorization" => enc_credentials },
+  #   }
+  # ).execute do |response, _request, _result|
+  #     case response.code
+  #     when 400
+  #       [:error, as_json(response)]
+  #     when 200
+  #       categories = JSON.parse(response.body.as_json)
+  #     else
+  #       fail "Invalid response #{response.as_json} received."
+  #     end
+  #   end
 
 
-    # * Pull 20 playlists of a certain category in spotify
-    category = RSpotify::Category.find(cat)
-    # tracks = uri.open()
-    playlists = category.playlists
-    # * pull 1 random playlist out of the collection of 20
-    playlist_response = playlists[rand(playlists.size) - 1].href
-    x = JSON.parse( URI.open(playlist_response, {'Authorization'=>"Bearer #{spotify_user.credentials['token']}", "Content-Type"=>"application/json"}).read)
-    y = x['tracks']['items'].map { |item| [item['track']['uri'], item['track']['duration_ms']] unless item.nil? }
-    # tracks = playlist_response.tracks.sample(1) unless playlist_response.nil? # * <--- RETURN SINGLE TRACK IN RANDOM POSITION
-    y.sample(1)
+    # TODO: retrieve the playlists from the category repsonse
+    # * get the category url
+    category_url =  "https://api.spotify.com/v1/browse/categories/pop"
+
+    # * get the playlist url from the category
+    playlist_data = JSON.parse(RestClient.get(category_url + "/playlists", { "Accept" => "application/json", "Content-Type" => "application/json", "Authorization" => enc_credentials }))
+
+    playlist_url =  playlist_data['playlists']['items'][rand(playlist_data.length)-1]['href']
+
+
+    song_data = JSON.parse(RestClient.get(playlist_url + "/tracks?&limit=1&offset=#{rand(20)}", { "Accept" => "application/json", "Content-Type" => "application/json", "Authorization" => enc_credentials }))
+
+    song = [song_data['items'].first['track']['uri'], song_data['items'].first['track']['duration_ms']]
   end
 
   def recipes_params
     params.require(:recipe).permit(:url)
   end
 
-  def find_user
-    spotify_user = RSpotify::User.new(request.env['omniauth.auth'])
-    user = User.find_by(email: spotify_user.email)
-    user_hash = JSON.parse(user.spotify_hash)
-    spotify_user = RSpotify::User.new(user_hash)
+  def store_user_cred
+    { access_token: spotify_user.credentials['token'],
+      refresh_token: spotify_user.credentials['refresh_token'],
+      time_of_creation: Time.now }
+  end
+
+  def spotify_urls
+   spotify_urls =  {
+      categories: "https://api.spotify.com/v1/browse/categories/",
+      token: "https://accounts.spotify.com/api/token",
+      refresh: 'https://api.spotify.com/v1/refresh'
+    }
   end
 end
