9297b1499144ddccafa7075a1363098a4acd71cc
Boyan Asenov <ivanoffboyan@gmail.com>
2022-06-07 13:03:14 +0200

    added favorite feature

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -27,6 +27,12 @@ class RecipesController < ApplicationController
     @instructions = @recipe.instructions
   end
 
+  def update
+    @recipe = Recipe.find(params[:id])
+    @recipe.update(favorite: true)
+    redirect_to recipe_path(@recipe)
+  end
+
   def destroy
     @recipes.destroy
     redirect_to recipes_path
