608ba15c16c9963e54ce9b8a778d25891880401a
Boyan Asenov <ivanoffboyan@gmail.com>
2022-06-08 17:33:27 +0200

    tap and hover instructions

---
+++ app/javascript/controllers/opacity_controller.js
@@ -0,0 +1,22 @@
+import { Controller } from "stimulus"
+
+export default class extends Controller {
+  static targets = [ "number", "check" ]
+
+  connect() {
+    console.log("hello")
+  }
+
+  change() {
+    this.element.classList.toggle('transparent-card')
+    this.numberTarget.classList.add('d-none')
+    this.checkTarget.classList.remove('d-none')
+  }
+
+  hover() {
+    if (!this.element.classList.contains("transparent-card")) {
+      this.numberTarget.classList.toggle('d-none')
+      this.checkTarget.classList.toggle('d-none')
+    }
+  }
+}
