0365e14ba5aa4eb86ee4a1b3a9fe6b6b1ed81383
eljopleqi <eljopleqi@gmail.com>
2022-06-13 16:04:19 +0200

    created the scaffolding for the components for the cookbook

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,6 +5,12 @@ import { AppComponent } from './app.component';
 import { LandingpageComponent } from './landingpage/landingpage.component';
 import { HeaderComponent } from './landingpage/header/header.component';
 import { SpotifyButtonComponent } from './landingpage/spotify-button/spotify-button.component';
+import { CookbookComponent } from './cookbook/cookbook.component';
+import { SidebarComponent } from './cookbook/sidebar/sidebar.component';
+import { InputFieldComponent } from './cookbook/input-field/input-field.component';
+import { RecipesComponent } from './cookbook/recipes/recipes.component';
+import { RecipeCardComponent } from './cookbook/recipes/recipe-card/recipe-card.component';
+import { UserInfoComponent } from './cookbook/sidebar/user-info/user-info.component';
 
 @NgModule({
   declarations: [
@@ -12,6 +18,12 @@ import { SpotifyButtonComponent } from './landingpage/spotify-button/spotify-but
     LandingpageComponent,
     HeaderComponent,
     SpotifyButtonComponent,
+    CookbookComponent,
+    SidebarComponent,
+    InputFieldComponent,
+    RecipesComponent,
+    RecipeCardComponent,
+    UserInfoComponent,
   ],
   imports: [BrowserModule],
   providers: [],
